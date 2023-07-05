81a6cdca511ba53618612e3e74495a20a25a7647
Agustina Maldonado <agus.maldonado11496@gmail.com>
2023-07-05 15:21:21 -0300

    Corrección switch

--- js/tareaswitch.js
+++ js/tareaswitch.js
@@ -1,5 +1,5 @@
 //En un establecimiento de juegos existen cuatro salas: Arcade, Consolas, Rol, Realidad Virtual. Si un usuario paga 4 créditos puede acceder a todas, si paga 3 solo podrá acceder a las tres primeras, si paga 2 a las dos primeras y si paga 1 solo a la primera sala.
-let creditos = parseFloat(prompt("Ingrese la cantidad de creditos"));
+let creditos = parseInt(prompt("Ingrese la cantidad de creditos"));
 
 switch (true) {
   case creditos < 1:
